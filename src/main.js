a4558bb2337eb1f91097fc772cb1931c682d3237
Yandy Gonzalez <yandygonzalez@gmail.com>
2018-05-06 16:49:55 -0400

    Add getRandonInt, getCelebrity functions

--- src/main.js
+++ src/main.js
@@ -2,5 +2,14 @@
 const importedCeleb = require("./celebrities.js"); // ES2016 import does't work on backend without express, babel, etc
 const celebrities = importedCeleb.exportCeleb();
 
-console.log(celebrities[0].country);
-console.log(celebrities[0].born);
+// Select a random number
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+// Select a random celebrity
+function getCelebrity() {
+  return celebrities[getRandomInt(celebrities.length)];
+}
+
+console.log(getCelebrity());
